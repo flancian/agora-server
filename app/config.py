fc75e469c0adcd2fae332dd6bdcadc11b5b35382
Flancian <0@flancia.org>
2020-11-10 22:48:20 +0100

    Some good progress, but also lightly broken code :)

--- app/config.py
+++ app/config.py
@@ -1,3 +1,7 @@
 import os
 import getpass
+
 AGORA_PATH = os.path.join('/home', getpass.getuser(), 'agora')
+
+# With trailing slash.
+URL_BASE = "https://anagora.org/"
--- app/db.py
+++ app/db.py
@@ -20,17 +20,75 @@ from operator import attrgetter
 
 RE_WIKILINKS = re.compile('\[\[(.*?)\]\]')
 
+# URIs are ids. 
+# - In the case of nodes, their [[wikilink]].
+#   - Example: 'foo', meaning the node that is rendered when you click on [[foo]] somewhere.
+# - In the case of subnodes, a relative path within the Agora.
+#   - Example: 'garden/flancian/README.md', meaning an actual file called README.md.
+#   - Note the example subnode above gets rendered in node [[README]], so fetching node with uri README would yield it (and others).
+
+# TODO: implement.
+class Graph:
+    def __init__(self):
+        # [[wikilink]] -> Node
+        self.nodes = {}
+        # node -> [n0, ..., nn] such that node has outlinks to the target list.
+        self.edges = {}
+    def addsubnode(self, subnode):
+        if subnode.wikilink in self.nodes:
+            G.nodes[subnode.wikilink].subnodes.append(subnode)
+        else:
+            G.nodes[subnode.wikilink] = Node(subnode.wikilink)
+
+G = Graph()
+
 class Node:
+    """Nodes map 1:1 to wikilinks.
+    They resolve to a series of subnodes when being rendered (see below).
+    It maps to a particular file in the Agora repository, stored (relative to 
+    the Agora root) in the attribute 'uri'."""
+    def __init__(self, wikilink):
+        # Use a node's URI as its identifier.
+        # Subnodes are attached to the node matching their wikilink.
+        # i.e. if two users contribute subnodes titled [[foo]], they both show up when querying node [[foo]].
+        self.wikilink = wikilink
+        self.uri = wikilink
+        self.url = '/node/' + self.uri
+        self.subnodes = []
+
+class Subnode:
+    """A subnode is a note or media resource volunteered by a user of the Agora.
+    It maps to a particular file in the Agora repository, stored (relative to 
+    the Agora root) in the attribute 'uri'."""
     def __init__(self, path):
-        self.dir = path_to_url(path)
+        # Use a subnode's URI as its identifier.
+        self.uri = path_to_uri(path)
+        self.url = '/subnode/' + path_to_uri(path)
+        # Subnodes are attached to the node matching their wikilink.
+        # i.e. if two users contribute subnodes titled [[foo]], they both show up when querying node [[foo]].
         self.wikilink = path_to_wikilink(path)
-        self.url = '/node/' + self.wikilink
+        self.user = path_to_user(path)
         with open(path) as f:
             self.content = f.read()
         self.outlinks = content_to_outlinks(self.content)
+        self.node = self.wikilink
+        # Initiate node for wikilink if this is the first subnode, append otherwise.
+        G.addsubnode(self)
 
-def path_to_url(path):
+class User:
+    def __init__(self, user):
+        self.uri = user
+        self.url = '/user/' + self.uri
+
+def path_to_uri(path):
     return path.replace(config.AGORA_PATH + '/', '')
+
+def path_to_user(path):
+    m = re.search('garden/(.+?)/', path)
+    if m:
+        return m.group(1)
+    else:
+        return 'agora'
 
 def path_to_wikilink(path):
     return os.path.splitext(os.path.basename(path))[0]
@@ -43,19 +101,46 @@ def content_to_outlinks(content):
     else:
         return []
 
-def all_nodes():
+def all_subnodes():
     l = sorted([f for f in glob.glob(os.path.join(config.AGORA_PATH, '**/*.md'), recursive=True)])
-    return [Node(f) for f in l]
+    return [Subnode(f) for f in l]
+
+def all_nodes():
+    # hack hack.
+    l = all_subnodes()
+    return sorted([Node(sn.wikilink) for sn in l], key=attrgetter('wikilink'))
+
+def all_users():
+    # hack hack.
+    users = os.listdir(os.path.join(config.AGORA_PATH, 'garden'))
+    return sorted([User(u) for u in users], key=attrgetter('uri'))
 
 def all_journals():
     # hack hack.
-    l = sorted([f for f in glob.glob(os.path.join(config.AGORA_PATH, '**/????-??-??.md'), recursive=True)])
-    return sorted([Node(f) for f in l], key=attrgetter('wikilink'), reverse=True)
+    l = all_subnodes()
+    # return sorted([sn for sn in l], key=attrgetter('wikilink'), reverse=True)
+    return sorted([f for f in glob.glob(os.path.join(config.AGORA_PATH, '**/????-??-??.md'), recursive=True)])
 
 def nodes_by_wikilink(wikilink):
     nodes = [node for node in all_nodes() if node.wikilink == wikilink]
     return nodes
 
+def subnodes_by_wikilink(wikilink):
+    subnodes = [subnode for subnode in all_subnodes() if subnode.wikilink == wikilink]
+    return subnodes
+
+def subnodes_by_user(user):
+    subnodes = [subnode for subnode in all_subnodes() if subnode.user == user]
+    return subnodes
+
+def subnode_by_uri(uri):
+    subnode = [subnode for subnode in all_subnodes() if subnode.uri == uri][0]
+    return subnode
+
 def nodes_by_outlink(wikilink):
     nodes = [node for node in all_nodes() if wikilink in node.outlinks]
     return nodes
+
+def subnodes_by_outlink(wikilink):
+    subnodes = [subnode for subnode in all_subnodes() if wikilink in subnode.outlinks]
+    return subnodes
