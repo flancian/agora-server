71f0a3c4e4df3390f5cdfa5e39fec74dedb66663
Nik V <github@nikvdp.com>
2020-11-27 19:18:51 -0800

    make journals in roam date format be recognized by journals page

--- app/util.py
+++ app/util.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import re
 
 def canonical_wikilink(wikilink):
     # hack hack
@@ -22,3 +23,19 @@ def canonical_wikilink(wikilink):
         .replace('/', '-')
     )
     return wikilink
+
+
+def is_journal(wikilink):
+    date_regexes = [
+        # iso format
+        '[0-9]{4}-[0-9]{2}-[0-9]{2}',
+        # roam format (what a monstrosity!)
+        '(January|February|March|April|May|June|July|August|September|October|November|December) [0-9]{1,2}(st|nd|th), [0-9]{4}',
+        # roam format (sanitzed for filenames)
+        '(january|february|march|april|may|june|july|august|september|october|november|december)-[0-9]{1,2}(st|nd|th)-[0-9]{4}',
+    ]
+
+    # combine all the date regexes into one super regex
+    combined_date_regex = re.compile(f'^({"|".join(date_regexes)})$')
+
+    return combined_date_regex.match(wikilink)
