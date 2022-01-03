cebada370f599f0c9d299b65cc7b8e540bb18dbf
Flancian <0@flancia.org>
2022-01-03 15:32:22 +0100

    Add simple Wikidata integration.

--- app/exec/wp.py
+++ app/exec/wp.py
@@ -20,16 +20,28 @@ import pprint
 def wp(node):
     search = requests.get(f'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={node}&format=json')
     pageid = search.json()['query']['search'][0]['pageid']
-    result = requests.get(f'https://en.wikipedia.org/w/api.php?action=query&pageids={pageid}&prop=extlinks|info&inprop=url&format=json').json()
+    result = requests.get(f'https://en.wikipedia.org/w/api.php?action=query&pageids={pageid}&prop=extlinks|info|pageprops&inprop=url&ppprop=wikibase_item&format=json').json()
     title = result['query']['pages'][str(pageid)]['title']
     url = result['query']['pages'][str(pageid)]['canonicalurl']
+    wikibase_item = result['query']['pages'][str(pageid)]['pageprops']['wikibase_item']
+    wikidata_url = f'https://www.wikidata.org/wiki/{wikibase_item}'
     inferred_node = title.replace('_', '-')
     return Response(f"""
-        <div class='exec'><ul><li><a href='/wp'>wp</a> → </a><a href='{url}'><strong>{url}</strong></a> 
+        <div class='exec'>
+        <ul>
+        <li><a href='/wp' title='Wikipedia search'>wp</a> → </a><a href='{url}'><strong>{url}</strong></a> 
         <button class='pull-exec' value='{url}'>pull</button>
+        <ul>
         <!-- <button class="go-exec" value="exec-wp">go</button>-->
-        <span class='node-hint'> → <a href='/{inferred_node}'>[[{title}]]</a></span>
-        </li></ul>
+        <li>Wikidata <a href='{wikidata_url}'>{wikibase_item}</a>
+        <button class='pull-exec' value='{wikidata_url}'>pull</button>
+        </li>
+        <li>Agora <a href='/{inferred_node}'>[[{title}]]</a>
+        <button class='pull-exec' value='/{inferred_node}'>pull</button>
+        </li>
+        </ul>
+        </li>
+        </ul>
         <!--{result}--></div>""", 
         mimetype='text/html'
     )
