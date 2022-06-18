ccd91d4deea9aa49354fe0a60990e8ecbda6cb20
Flancian <0@flancia.org>
2022-06-18 10:28:17 -0400

    Fix setup.sh, check for npm and esbuild.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-(command -v python3 && command -v pip3) || (echo 'Please install Python3 and pip using your OS packaging system. In Debian: sudo apt-get install python3 python3-venv python3-pip' && exit 42)
+ERROR=0
+
+if ! command -v python3 || ! command -v pip3; then
+    echo 'Please install Python3 and pip using your OS packaging system. In Debian: sudo apt-get install python3 python3-venv python3-pip'
+    exit 42
+fi
+
+
+if ! command -v npm || ! command -v esbuild; then
+    echo 'Please install npm and esbuild using your OS packaging system (this is needed for client development). In Debian: sudo apt-get install npm esbuild' 
+    exit 43
+fi
 
 python3 -m venv venv &&
 . venv/bin/activate &&
