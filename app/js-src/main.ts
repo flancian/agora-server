95cd27f58ad351b7c73403a272d3c06084d9fc9e
Flancian <0@flancia.org>
2022-11-05 16:17:26 +0100

    Aesthetic fix.

--- app/js-src/main.ts
+++ app/js-src/main.ts
@@ -506,28 +506,26 @@ document.addEventListener("DOMContentLoaded", function () {
         if (!this.classList.contains('pulled')) {
           this.innerText = 'autopulling in empty node...';
         }
+      });
 
-    // hack hack
-    // this is copy/pasted from the first bind, as for some reason we need to re-bind after insert/remove.
-    $("#pull-stoa").click(function (e) {
-      console.log('clicked stoa button')
-      if (this.classList.contains('pulled')) {
-        // already pulled.
-        this.innerText = 'pull';
-        $(e.currentTarget).nextAll('iframe').remove()
-        $("#stoa-iframe").html('');
-        this.classList.remove('pulled');
-      }
-      else {
-        this.innerText = 'pulling';
-        let node = this.value;
-        $("#stoa-iframe").html('<iframe id="stoa-iframe" name="embed_readwrite" src="https://doc.anagora.org/' + node + '?edit" width="100%" height="500" frameborder="0"></iframe>');
-        this.innerText = 'fold';
-        this.classList.add('pulled');
-      }
-    });
-
-
+      // hack hack
+      // this is copy/pasted from the first bind, as for some reason we need to re-bind after insert/remove.
+      $("#pull-stoa").click(function (e) {
+        console.log('clicked stoa button')
+        if (this.classList.contains('pulled')) {
+          // already pulled.
+          this.innerText = 'pull';
+          $(e.currentTarget).nextAll('iframe').remove()
+          $("#stoa-iframe").html('');
+          this.classList.remove('pulled');
+        }
+        else {
+          this.innerText = 'pulling';
+          let node = this.value;
+          $("#stoa-iframe").html('<iframe id="stoa-iframe" name="embed_readwrite" src="https://doc.anagora.org/' + node + '?edit" width="100%" height="500" frameborder="0"></iframe>');
+          this.innerText = 'fold';
+          this.classList.add('pulled');
+        }
       });
       setTimeout(autoPullStoa2, 2000);
     }
