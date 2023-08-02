b64763edad02f31c3db5a6f38dc31312a8f1b72b
Flancian <0@flancia.org>
2023-08-02 20:17:23 +0200

    Trying to allow Agora setting overrides from sources.yaml :)

--- app/config.py
+++ app/config.py
@@ -20,22 +20,44 @@ class DefaultConfig(object):
     # note: I don't like being first among the humans, I still do it because my subnodes are often of the summary/tldr kind whenever they overlap with others.
     # I'd like to relinquish control of ranking functions in favour of [[flancia collective]], [[agora discuss]] in that order.
     # Jayu temporarily out as they left the Agora while rebooting their garden.
-    RANK = ['agora', 'flancian', 'vera', 'neil', 'maya', 'j0lms']
+    # RANK = ['agora', 'flancian', 'vera', 'neil', 'maya', 'j0lms']
+
+    # Uprank the system user only (again).
+    RANK = ['agora']
+
     # deprecated/check if unused
     AGORA_VERSION = '0.9'
-    # standard: no trailing slashes anywhere in variables.
-    # with protocol
-    URL_BASE = "https://anagora.org"
-    API_BASE = "https://api.anagora.org"
-    # https://anagora.org, the reference Agora as of 2022, is run by [[Flancia Collective]].
-    # See https://anagora.org/agora+doc for more.
-    NAME = "Agora of Flancia"
+
+    try:
+        NAME = YAML_CONFIG['agora_name']
+    except KeyError:
+        # Just a hopefully sane, well intentioned default ;)
+        NAME = "Agora of Flancia"
+        # https://anagora.org, the reference Agora as of 2022, is run by [[Flancia Collective]].
+        # See https://anagora.org/agora+doc for more.
+
+    try:
+        URL_BASE = YAML_CONFIG['url_base']
+    except KeyError:
+        # standard: no trailing slashes anywhere in variables.
+        # with protocol
+        URL_BASE = "https://anagora.org"
+
+    try:
+        URI_BASE = YAML_CONFIG['uri_base']
+    except KeyError:
+        URI_BASE = "anagora.org"
+
+    try:
+        API_BASE = YAML_CONFIG['api_base']
+    except KeyError:
+        API_BASE = "https://api.anagora.org"
+
+    AGORA = URI_BASE
 
     # change this to whatever your domain is going to be -- without protocol.
     # this is what gets rendered in the header.
     # 2022-12-02: maybe deprecated in favour of using request headers to infer the host that the client wants to see? see before_request in agora.py. 
-    URI_BASE = "anagora.org"
-    AGORA = URI_BASE
     JOURNAL_ENTRIES = 31 # number of journal entries to load
     
     # EXPERIMENTS
@@ -55,10 +77,13 @@ class ProductionConfig(DefaultConfig):
     ENABLE_AUTO_PULL = True
     ENABLE_AUTO_STOA = False
 
+
 class DevelopmentConfig(DefaultConfig):
     URL_BASE = "http://dev.anagora.org"
     URI_BASE = "dev.anagora.org"
     API_BASE = "http://localhost:3000"
+
+    NAME = "Alpha Agora of Flancia"
 
     # EXPERIMENTS
     ENABLE_CTZN = True
@@ -72,6 +97,7 @@ class LocalDevelopmentConfig(DefaultConfig):
     URI_BASE = "localhost:5017"
     API_BASE = "http://localhost:3000"
 
+    NAME = "Development Agora of Flancia"
     # EXPERIMENTS
     ENABLE_CTZN = True
     ENABLE_STATS = True
