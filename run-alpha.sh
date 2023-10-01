4285e675e789f38424475f25895ac785f102e068
Flancian <0@flancia.org>
2023-10-01 15:20:14 +0200

    Add new run-alpha.sh designed for the experimental Agora of Flancia :)

---
+++ run-alpha.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# npm install ./app/js-src --prefix ./app/js-src
+# npx parcel-bundler build app/js-src/*.js --out-dir ../static/js
+# . venv/bin/activate
+# parcel watch app/js-src/index.ts --dist-dir app/static/js &
+#
+# This shouldn't be needed but it is when running as a systemd service for some reason.
+export PATH=$HOME/.local/bin:${PATH}
+
+npm run build
+export FLASK_ENV="production"
+
+echo "This enables maybe dangerous flags like executable subnodes.\nUse at your own risk.\nUse ./run-prod.sh for safer defaults."
+
+export AGORA_CONFIG="AlphaConfig"
+poetry run uwsgi prod.ini
