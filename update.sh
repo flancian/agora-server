58010261e1e6eef8680088c654ab22602c183857
Agora v0.5 <agora@anagora.org>
2023-12-18 21:34:26 +0100

    poetry install when updating, will save outages probably.

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@
 # Maybe not useful to others? But useful to update the 'bare' setup in anagora.org :)
 
 git pull
+poetry install
 npm run build
 touch /tmp/agora-restart
 sudo /bin/sh -c 'rm /tmp/agora-cache/*'
