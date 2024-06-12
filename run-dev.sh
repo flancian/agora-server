23360656792115934b8570940c0ce11b7392c7ba
Flancian <0@flancia.org>
2024-06-12 21:30:28 +0200

    Make dev agora-server systemd-with-poetry compatible :)
    Also a stub for edit.anagora.org

--- run-dev.sh
+++ run-dev.sh
@@ -20,4 +20,6 @@ npm run build
 export FLASK_APP=app
 export FLASK_ENV="development"
 export AGORA_CONFIG="${1}DevelopmentConfig"
+# This shouldn't be needed but it is when running as a systemd service for some reason.
+export PATH=$HOME/.local/bin:${PATH}
 poetry run flask --debug run -h 0.0.0.0 -p 5017
