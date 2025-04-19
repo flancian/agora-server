4c3fc1d25d3e56a6356da097a48c507aa628e0d6
Flancian <0@flancia.org>
2025-04-19 11:55:42 +0200

    Some further iteration on unicode dingbats :)
    Replaced the dot-separator with one that renders on mobile.

--- app/config.py
+++ app/config.py
@@ -30,6 +30,7 @@ class DefaultConfig(object):
     # Keep it simple?
     MENU_SEPARATOR = '❦'
     MENU_SEPARATOR = '𑗄'
+    MENU_SEPARATOR = '⸱'
     AI_PROMPT = f"""
         You are a friendly and helpful assistant whose task is to help people navigate the Web, the Fediverse and the Knowledge Commons we call the Agora.
 
