626c2bc05e515a8647d7e962f19d59d6ad0628e9
Agora v0.5 <agora@anagora.org>
2025-04-22 01:45:14 +0200

    Remove call to poetry, we've moved to uv and I don't think it needs an install step

--- update.sh
+++ update.sh
@@ -3,7 +3,6 @@
 # Maybe not useful to others? But useful to update the 'bare' setup in anagora.org :)
 
 git pull
-poetry install
 npm run build
 touch /tmp/agora-restart
 sudo /bin/sh -c 'rm /tmp/agora-cache/*'
