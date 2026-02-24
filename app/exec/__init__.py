4d00b5c4ae61366b09d396c5c8f5cf1df28e4a22
Flancian <0@flancia.org>
2026-02-24 18:22:02 +0100

    Fix: Restore Response re-export in app/exec/__init__.py to fix ImportError

--- app/exec/__init__.py
+++ app/exec/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask import Blueprint
+from flask import Blueprint, Response
 
 bp = Blueprint("exec", __name__)
 
